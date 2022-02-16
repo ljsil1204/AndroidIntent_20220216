cd1ff081df09f3470351accf4a9ecd636586a2e3
이진실 <ljsil1204@naver.com>
2022-02-16 11:47:49 +0900

    플레이 스토어로 바로 링크

--- app/src/main/java/com/neppplus/androidintent_20220216/MainActivity.kt
+++ app/src/main/java/com/neppplus/androidintent_20220216/MainActivity.kt
@@ -57,5 +57,13 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        btnKokaoTalkPlayStoreLink.setOnClickListener {
+
+            val myUri = Uri.parse("market://details?id=com.kakao.talk")
+            val myIntent =Intent(Intent.ACTION_VIEW, myUri)
+            startActivity(myIntent)
+
+        }
+
     }
 }
